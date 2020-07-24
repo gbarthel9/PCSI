b37ebb95805b902ea72c37ec64e7dc30b2d90013
Grant Barthelmes <gbarthel@howard.virtual.crc.nd.edu>
2020-07-24 15:29:15 -0400

    Exports data using cvs package, might not be the best method but it quasi works, check with Howard

--- ssimDataProg.py
+++ ssimDataProg.py
@@ -5,6 +5,7 @@ import argparse
 from pcsiSimulatorFunc import pcsiSimulator
 import numpy as np
 from scipy.optimize import minimize
+import csv
 
 def costFunc(imgOrig, cc, b, numP, ba, outfolder, saveImages): #Where imgOrig, ba, outfolder, and saveImages are parameters not variables
     # Returns array of image data 
@@ -21,39 +22,37 @@ def main():
                         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("-i", "--imagefile", type=str, default='HAB2sstv.bmp',
                         help="Input image to transmit (24bit color, any filetype)")
-    parser.add_argument("-b", "--bitdepthrange", type=int, default=[12,24,3], nargs='*',
+    parser.add_argument("-b", "--bitdepthrange", type=int, default=[21,21,3], nargs='*',
                         help="Bit depth range to test and iteration step; should be mult of 3 (enter 9 24 3 for 9,12,...24)")
-    parser.add_argument("-N", "--numpacketsrange", type=int, nargs='*', default=[100,400,100],
+    parser.add_argument("-N", "--numpacketsrange", type=int, nargs='*', default=[100,100,100],
                         help="Range of packet quantity to test and iteration step ")
-    parser.add_argument("-c", "--chromacomprange", type=int, default=[16, 24, 4], nargs='*',
+    parser.add_argument("-c", "--chromacomprange", type=int, default=[20, 20, 4], nargs='*',
                         help="Chroma Compression ratio range to test and iteration step")
     parser.add_argument("-a", "--bitsAvailable", type=int, default=1992,
                         help="Number of bits available in payload for image data")
     args = parser.parse_args()
 
     #NOTE: Range operates on integers only
-    bdr = range(args.bitdepthrange[0], args.bitdepthrange[1]+1, args.bitdepthrange[2])
-    npr = range(args.numpacketsrange[0], args.numpacketsrange[1]+1, args.numpacketsrange[2])
-    ccr = range(args.chromacomprange[0], args.chromacomprange[1]+1, args.chromacomprange[2])
+    bdr = list(range(args.bitdepthrange[0], args.bitdepthrange[1]+1, args.bitdepthrange[2]))
+    npr = list(range(args.numpacketsrange[0], args.numpacketsrange[1]+1, args.numpacketsrange[2]))
+    ccr = list(range(args.chromacomprange[0], args.chromacomprange[1]+1, args.chromacomprange[2]))
     
     ssimDataMtrx = np.zeros((len(bdr), len(npr), len(ccr)))
-    print(ssimDataMtrx)
+    #print(ssimDataMtrx)
     #Might want to use map in the future so it can be more concurrent for parallelizaiton
     for i, b in enumerate(bdr):
         for j, numP in enumerate(npr):
             for k, cc in enumerate(ccr):
                 ssimDataMtrx[i][j][k] = costFunc(args.imagefile, b, [numP],[cc], args.bitsAvailable, "results_NA", False)
-    print(ssimDataMtrx)
+    #print(ssimDataMtrx)    
     
-
-    #Test:
-    #s10 = costFunc(args.imagefile, 24, [300], [4], args.bitsAvailable, "results_NA", False)
-    #s11 = costFunc(args.imagefile, 12, [150],[ 4], args.bitsAvailable, "results_NA", False)
-    #print(f"s10 is: {s10}")
-    #print(f"s11 is: {s11}")
-    #print(f"s20 is: {s20}")
-    #print(f"s21 is: {s21}")
-
+    with open("ssimData.csv", 'w') as csvfile:
+        csvfile.write(f'Bit Depth Values: {bdr}\n')
+        csvfile.write(f'NumPackets Values: {npr}\n')
+        csvfile.write(f'CC Values: {ccr}\n')
+        csvfile.write('SSIM Data:\n')
+        writer = csv.writer(csvfile, delimiter=",")
+        writer.writerows(ssimDataMtrx)
 
 if __name__ == "__main__":
     main()
